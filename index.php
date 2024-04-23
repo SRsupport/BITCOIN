a120c1ca37815b3d6ad853b6656a662134590adc
SRsupport <167115124+SRsupport@users.noreply.github.com>
2024-04-23 21:36:16 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>News bitcoin - BR </title>
+      <link rel="icon" type="image/x-icon" href="assets/faviconb.ico">
+      <link rel="stylesheet" href="assets/main.css">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.js"></script>
+      <script src="assets/dr-dtime.js"></script>
+   </head>
+   <body>
+      <div class="main">
+         <div class="wrapper">
+            <a href="#" target="_blank">  <img  src="assets/1606212379399.jpg" alt="" class="img"></a>
+            <h2>
+               <script language="javascript">dtimes(0)</script>: O Bitcoin oferece ganhos impressionantes à medida que mais brasileiros se engajam.
+            </h2>
+            <div class="smal_text">
+               Publicado em 
+               <span>
+                  <script type="text/javascript">dtime_nums(-1, true)</script>
+               </span>
+            </div>
+            <div class="comment flex">
+               <div class="flex">
+                  <a href="#" target="_blank" class="icon"><img  src="assets/1605636244972.jpg" alt=""></a><a href="#" target="_blank">
+                  </a>
+                  <div>
+                     <a href="#" target="_blank">
+                     </a>
+                     <div class="com_top"><a href="#" target="_blank">
+                        </a><a href="#" target="_blank" class="name">Lucas Sousa</a>
+                        <a href="#" target="_blank" class="follow">Follow</a>
+                     </div>
+                     <a href="#" target="_blank" class="com_bot">Owner at Freelance, self-employed</a>
+                  </div>
+               </div>
+               <div class="flex">
+                  <a href="#" target="_blank" class="flex marg  otstup"> <img  src="assets/like.png" alt=""><span>812</span></a>
+                  <a href="#" target="_blank" class="flex  otstup"> <img  src="assets/com.png" alt=""><span>0</span></a>
+               </div>
+            </div>
+            <div class="data">
+               <script language="javascript">dtimes(0, true)</script> 10:29
+            </div>
+
+            <div class="text">
+               <p> <a href="#" target="_blank">O Bitcoin</a>,
+                  a criptomoeda digital, é a moeda digital mais recente do mundo. Esta é a
+                  primeira moeda do gênero apoiada e promovida pelo país. Os investidores
+                  profissionais investem pesadamente na esperança de aumentar seu valor 
+                  milhares de vezes.
+               </p>
+               <a href="#" target="_blank"> <img  src="assets/1606212448394.jpg" alt="" class="img"></a>
+               <p>"Investment analysis" conduziu uma pesquisa 
+                  independente entrevistando mais de 26.000 investidores em todo o mundo. 
+                  Ele descobriu que mais de 93,8% estavam considerando investir no 
+                  Bitcoin, e mais de 70% deles eram novos investidores.
+               </p>
+               <p>A pesquisa mostra que o Bitcoin é uma das moedas digitais mais cobiçadas, que crescerá milhares de vezes no futuro próximo.</p>
+               <p>O estudo também destaca a recente mudança em 
+                  direção a investimentos exóticos. O mercado de ações atingiu novos 
+                  máximos e não pode mais fornecer os retornos exigidos por muitos 
+                  investidores. Portanto, a <a href="#" target="_blank">Bitcoin atraiu investidores 10 vezes o dinheiro investido apenas no mês de outubro.</a>
+               </p>
+               <p>“Com o apoio oficial do banco central, o risco de
+                  colapso é baixo e as atuais condições de mercado são ideais para 
+                  investimento. Os principais índices de ações estão caindo e muitos de 
+                  nós estão visando o <b>Bitcoin para retornos</b> recordes. Esta é a nossa 
+                  oportunidade de investir cedo e não perder outra oportunidade semelhante
+                  ao Bitcoin ”, disse um corretor de fundos de hedge sênior, que pediu 
+                  anonimato, em uma entrevista.
+               </p>
+               <p>“É realmente impossível resistir a essa 
+                  oportunidade de investimento. É a primeira moeda digital suportada por 
+                  todos os países. A Brasil dá à luz <a href="#" target="_blank"> um novo bilionário a cada semana e seus mercados estão aquecidos</a>Tem muito capital e liquidez, então não dá para olhar para trás ”, diz Faustino Mangas, conhecido corretor de câmbio. 
+               </p>
+                 <a href="#" target="_blank" class="get_offer_link" style="margin: 0 auto;display: block;text-align: center;color:#fff;">Clique aqui para se Cadastrar</a>
+               <p>O Bitcoin passou de menos de $0,000764 para 
+                  $27.038, o que representa um investimento de $100 agora no valor de $5 
+                  milhões. É fácil perceber porque todos querem aproveitar <a href="#" target="_blank"> esta oportunidade.</a>
+               </p>
+               <p>Além disso, o Bitcoin pode ser uma solução para o
+                  problema da inadimplência de longo prazo da Brazil. Atualmente, o país 
+                  tem 293 milhões de euros em inadimplência devido à prática ilegal, 
+                  embora popular, de obter vários empréstimos com a mesma garantia. A 
+                  ampla adoção do Bitcoin na economia <b>brasileira aumentará ainda</b> mais seu 
+                  valor.
+               </p>
+               <p>“Usando contratos inteligentes fornecidos pela 
+                  moeda Bitcoin, o governo brasileiro será capaz de rastrear ativos e 
+                  passivos e garantir que vários empréstimos não sejam feitos com a mesma 
+                  garantia”, diz o pesquisador sênior de blockchain A. Ortiz.
+               </p>
+               <p>ser realizadas por meio <a href="#" target="_blank">de agentes autorizados.</a></p>
+               <p>Muitos investidores têm procurado ações de alto 
+                  potencial após a recente queda relacionada à crise e, para muitos, esta 
+                  pode ser uma grande oportunidade. Bitcoin pode não apenas resolver os 
+                  problemas de longa data da Brasil, mas também permite que muitas pessoas
+                  <b>da classe média subam na</b> escada financeira por meio de uma estratégia 
+                  de investimento segura.
+               </p>
+               <p>Se você deseja investir ou saber mais sobre o Bitcoin, <a href="#" target="_blank"> clique aqui.</a></p>
+
+               <a href="#" target="_blank" class="get_offer_link" style="margin: 0 auto;display: block;text-align: center;color:#fff;">Clique aqui para se Cadastrar</a>
+               <div class="comment flex">
+                  <div class="flex">
+                     <div class="flex flex_fix">
+                        <span class="razmesh">Publicado</span>
+                        <div class="flex ">
+                           <a href="#" target="_blank" class="icon"><img  src="assets/1605636244972.jpg" alt=""></a>
+                           <div>
+                              <div class="com_top">
+                                 <a href="#" target="_blank" class="name">Lucas Sousa</a>
+                              </div>
+                              <a href="#" target="_blank" class="com_bot">Owner at Freelance, self-employed</a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="flex">
+                     <a href="#" target="_blank" class="btn"> Follow</a>
+                  </div>
+               </div>
+               <div class="block flex flex_new">
+                  <div class="item flex flex_new">
+                     <a href="#" target="_blank"><img  src="assets/new_like.png" alt=""></a> 
+                     <span>Gostar</span>
+                  </div>
+                  <div class="flex flex_new">
+                     <a href="#" target="_blank"><img  src="assets/new_com.png" alt=""></a>
+                     <span>Comente</span>
+                  </div>
+               </div>
+               <div class="new_comment">
+                  <span>0 comentários</span>
+                  <div class="flex">
+                     <a href="#" target="_blank" class="icon"><img  src="assets/efkb5179rslll10nmhystl3wx.svg" alt=""></a>
+                     <div class="block_input">
+                        <input type="text" placeholder="Adicione um comentário…">
+                        <a href="#" target="_blank"><img  src="assets/smile.png" alt=""></a>
+                        <a href="#" target="_blank"><img  src="assets/img.png" alt=""></a> 
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <style type="text/css">
+         .get_offer_link {
+    width: 100%;
+border-radius: 10px;
+height: 45px;
+line-height: 25px;
+background-color: #8BBF3A;
+text-align: center;
+color: #ffffff;
+font-weight: bold;
+font-size: 18px;
+text-decoration: none;
+line-height: 2.5;
+}
+      </style>
+   </body>
+</html>
